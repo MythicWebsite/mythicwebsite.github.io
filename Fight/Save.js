b0a13ac316ab4385a6367cd7870080458fe117a3
MythicWebsite <78496235+MythicWebsite@users.noreply.github.com>
2021-02-10 08:17:30 -0500

    update Save.js

--- Fight/Save.js
+++ Fight/Save.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-	var player = new Object()
+	var player = new Object({});
 	var playername = null
 	var aName = null
 	console.log(NPC.Ted.int)
 	//Functions
 	function savedata(loc,thedata) {
 		localStorage.setItem(loc, JSON.stringify(thedata))
-	};
+	}
 	function loaddata(loc) {
 		player = JSON.parse(localStorage.getItem(loc))
-	};
+	}
 	function getE(ele) {
 		return document.getElementById(ele)
-	};
+	}
 
 	//Actual content
 	getE("input").focus()
@@ -21,7 +21,7 @@ $(document).ready(function() {
 		if (event.keyCode === 13) {
 			event.preventDefault();
 			getE("load").click();
-		};
+		}
 	});
 	$("#save").click(function(){
 		console.log("Player saved " + playername + ".")
