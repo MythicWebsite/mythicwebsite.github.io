d300165590ef43c44abba5a90ebb8e2e03d5cf59
MythicWebsite <78496235+MythicWebsite@users.noreply.github.com>
2021-02-10 17:01:18 -0500

    a

--- Game/Game.js
+++ Game/Game.js
@@ -53,7 +53,7 @@ $(document).ready(function() {
 			getE("loaddiv").style.display = "none";
 			getE("gametext").style.display = "block";
 			getE("gamediv").style.display = "block";
-			getE("guycontainer").style.display = "inline-block";
+			getE("guycontainer").style.display = "block";
 			getE("gameheader").innerHTML = aName + "'s game";
 			getE("gametext").innerHTML = aName + "'s game has been loaded.";
 		}
